0dfe1c29415594b027f070ed40ceed752f8899cf
livia-siqueira <livia.siqueira12@outlook.com>
2022-06-07 11:47:26 -0300

    fix: add limit in image

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,10 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
+app.use(express.json({ limit: "50mb" }));
 
 app.use(routes);
 
